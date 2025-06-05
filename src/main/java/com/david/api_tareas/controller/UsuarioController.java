2d1e5f94a557c7eecd06ea23106bde22c91ec2ed
David Fdez <dferna40@gmail.com>
2025-06-06 01:56:17 +0200

    Correcciones

--- src/main/java/com/david/api_tareas/controller/UsuarioController.java
+++ src/main/java/com/david/api_tareas/controller/UsuarioController.java
@@ -5,18 +5,14 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.david.api_tareas.dto.input.UsuarioInputDTO;
 import com.david.api_tareas.dto.output.UsuarioOutputDTO;
 import com.david.api_tareas.service.UsuarioService;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.validation.Valid;
 
 @RestController // Define esta clase como un controlador REST
 @RequestMapping("/usuarios") // URL base para este controlador
--- src/main/java/com/david/api_tareas/service/UsuarioService.java
+++ src/main/java/com/david/api_tareas/service/UsuarioService.java
@@ -6,10 +6,8 @@ import java.util.stream.Collectors;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import com.david.api_tareas.dto.input.UsuarioInputDTO;
 import com.david.api_tareas.dto.output.UsuarioOutputDTO;
 import com.david.api_tareas.mapper.UsuarioMapper;
-import com.david.api_tareas.model.Usuario;
 import com.david.api_tareas.repository.UsuarioRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -22,21 +20,21 @@ public class UsuarioService {
     private final UsuarioRepository usuarioRepository;
     private final PasswordEncoder passwordEncoder;
 
-    // Método para crear un nuevo usuario
-    public UsuarioOutputDTO crearUsuario(UsuarioInputDTO dto) {
-        // Convertimos el DTO de entrada a entidad
-        Usuario usuario = UsuarioMapper.toEntity(dto);
-
-        // Encriptamos la contraseña antes de guardarla
-        String passwordEncriptado = passwordEncoder.encode(usuario.getPassword());
-        usuario.setPassword(passwordEncriptado);
-
-        // Guardamos el usuario en la base de datos
-        Usuario usuarioGuardado = usuarioRepository.save(usuario);
-
-        // Convertimos la entidad guardada a DTO de salida
-        return UsuarioMapper.toDTO(usuarioGuardado);
-    }
+//    // Método para crear un nuevo usuario
+//    public UsuarioOutputDTO crearUsuario(UsuarioInputDTO dto) {
+//        // Convertimos el DTO de entrada a entidad
+//        Usuario usuario = UsuarioMapper.toEntity(dto);
+//
+//        // Encriptamos la contraseña antes de guardarla
+//        String passwordEncriptado = passwordEncoder.encode(usuario.getPassword());
+//        usuario.setPassword(passwordEncriptado);
+//
+//        // Guardamos el usuario en la base de datos
+//        Usuario usuarioGuardado = usuarioRepository.save(usuario);
+//
+//        // Convertimos la entidad guardada a DTO de salida
+//        return UsuarioMapper.toDTO(usuarioGuardado);
+//    }
 
     // Método para obtener todos los usuarios como DTOs de salida
     public List<UsuarioOutputDTO> obtenerTodosLosUsuarios() {
